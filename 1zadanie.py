02b2a3e8e9f00b1bcfe236c194281b4b91d334a0
kng111 <92807808+kng111@users.noreply.github.com>
2021-10-21 13:09:04 +0300

    Update 1zadanie.py

--- 1zadanie.py
+++ 1zadanie.py
@@ -2,11 +2,15 @@
 
 import random
 import arcade
+import time
+from arcade import key
+import speech_recognition as sr
+mic = sr.Microphone(device_index=1)
 
 SPRITE_SCALING = 0.5
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 700
 SCREEN_TITLE = "game"
 
 MOVEMENT_SPEED = 5
@@ -14,12 +18,10 @@ x = ['ladybug.png','frog.png','mouse.png','fly.png' ,'slimeBlue.png']
 x2 = (random.choice(x))
 
 class Player(arcade.Sprite):
-    """ Player Class """
+
 
     def update(self):
-        """ Move the player """
-        # Move player.
-        # Remove these lines if physics engine is moving player.
+
         self.center_x += self.change_x
         self.center_y += self.change_y
 
@@ -40,63 +42,88 @@ class MyGame(arcade.Window):
 
     def __init__(self, width, height, title):
 
-        # Call the parent class initializer
         super().__init__(width, height, title)
 
-        # Variables that will hold sprite lists
-        self.player_list = None
 
-        # Set up the player info
+        self.player_list = None
         self.player_sprite = None
 
-        # Set the background color
         arcade.set_background_color(arcade.color.KHAKI)
 
     def setup(self):
 
-        # Sprite lists
         self.player_list = arcade.SpriteList()
 
-        # Set up the player
         self.player_sprite = Player(f":resources:images/enemies/{x2}", SPRITE_SCALING)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
+        self.player_sprite.center_x = 500
+        self.player_sprite.center_y = 500
         self.player_list.append(self.player_sprite)
 
     def on_draw(self):
 
-        # This command has to happen before we start drawing
+
         arcade.start_render()
 
-        # Draw all the sprites.
         self.player_list.draw()
 
     def on_update(self, delta_time):
-
-        # Move the player
         self.player_list.update()
 
+    def voice():
+            r = sr.Recognizer()
+            with mic as source:
+                print('Слушаю...')
+                audio = r.listen(source)
+                    
+                    
+                query = r.recognize_google(audio,language = 'ru-RU')
+                print(f'вы сказали:{query.lower()}')
+                f = open('txt.txt','w')
+                f.write(query.lower())
+                f.close()
+                key = query.lower()
+    
+
+
     def on_key_press(self, key, modifiers):
+        # while True:
+            r = sr.Recognizer()
+            with mic as source:
+                print('Слушаю...')
+                audio = r.listen(source)
+                    
+                    
+                query = r.recognize_google(audio,language = 'ru-RU')
+                print(f'вы сказали:{query.lower()}')
+                f = open('txt.txt','w')
+                f.write(query.lower())
+                f.close()
+                key = query.lower()
+                if key == 'вверх':
+                    self.player_sprite.change_y = MOVEMENT_SPEED
+                    self.player_sprite.change_x = 0
 
-        # If the player presses a key, update the speed
-        if key == arcade.key.UP:
-            self.player_sprite.change_y = MOVEMENT_SPEED
-        elif key == arcade.key.DOWN:
-            self.player_sprite.change_y = -MOVEMENT_SPEED
-        elif key == arcade.key.LEFT:
-            self.player_sprite.change_x = -MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT:
-            self.player_sprite.change_x = MOVEMENT_SPEED
+                elif key == 'вниз':
+                    self.player_sprite.change_y = -MOVEMENT_SPEED
+                    self.player_sprite.change_x = 0
 
+                elif key == 'слева':
+                    self.player_sprite.change_x = -MOVEMENT_SPEED
+                    self.player_sprite.change_y = 0
+
+                elif key == 'справа':
+                    self.player_sprite.change_x = MOVEMENT_SPEED
+                    self.player_sprite.change_y = 0
+                elif key == 'стоп':
+                    self.player_sprite.change_y = 0
+                    self.player_sprite.change_x = 0
     def on_key_release(self, key, modifiers):
-
-        # If a player releases a key, zero out the speed.
-        # This doesn't work well if multiple keys are pressed.
-        # Use 'better move by keyboard' example if you need to
-        # handle this.
-        if key == arcade.key.UP or key == arcade.key.DOWN:
+        if key == 'вверх' or key == 'вниз':
             self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+        elif key == 'слева' or key == 'справа':
+            self.player_sprite.change_x = 0
+        elif key == 'стоп':
+            self.player_sprite.change_y = 0
             self.player_sprite.change_x = 0
 
 
@@ -108,6 +135,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
